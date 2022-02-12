549611ef5527c3b216c21ff6ce31423a2dc19ae5
aymammeri <ay.mammeri@gmail.com>
2022-02-12 17:33:30 -0500

    Map Columns and pass Modal to Sub component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Col, Stack, DropdownButton, Dropdown } from 'react-bootstrap'
+// import { createCell } from '../../api/crud.js'
 import Cell from '../Cell/Cell.js'
 
-const Checkpoint = props => {
+const Column = props => {
+  const { id, cells, setModalType, setModalProps, handleShow } = props
+  const [cellsJSX, setCellsJSX] = useState(null)
+
+  useEffect(() => {
+    setCellsJSX(
+      cells.map(cell => (
+        <Cell
+          key={cells.indexOf(cell)}
+          id={cell._id}
+          column={id}
+          setModalType={setModalType}
+          setModalProps={setModalProps}
+          handleShow={handleShow}
+        />
+      ))
+    )
+  }, [])
+
   return (
-    <Col className='column border border-dark fluid m-3 pb-3'>
+    <Col id={id} className='column border border-dark fluid m-3 pb-3'>
       <Stack className='d-flex'>
         <div className='text-center pt-2'>Title</div>
-        <DropdownButton className='ms-auto' variant='link' id='dropdown-basic-button' title={<img src='https://icongr.am/entypo/dots-three-horizontal.svg?size=14&color=currentColor'/>}>
-          <Dropdown.Item href='#/action-1'>Add</Dropdown.Item>
-          <Dropdown.Item href='#/action-1'>Rename</Dropdown.Item>
-          <Dropdown.Item href='#/action-2'>Delete</Dropdown.Item>
+        <DropdownButton
+          className='ms-auto'
+          variant='link'
+          id='dropdown-basic-button'
+          title={
+            <img src='https://icongr.am/entypo/dots-three-horizontal.svg?size=14&color=currentColor' />
+          }
+        >
+          <Dropdown.Item
+            onClick={() => {
+              setModalType('edit-column')
+              handleShow()
+            }}
+          >
+            Rename
+          </Dropdown.Item>
+          <Dropdown.Item>Delete</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item
+            onClick={() => {
+              setModalType('create-cell')
+              handleShow()
+            }}
+          >
+            New Cell
+          </Dropdown.Item>
         </DropdownButton>
       </Stack>
-      <Cell />
-      <Cell />
-      <Cell />
+      {cellsJSX}
     </Col>
   )
 }
 
-export default Checkpoint
+export default Column
