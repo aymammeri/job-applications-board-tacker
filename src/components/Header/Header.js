ba120d71d8325db06ff0de9b567b1a4d6d448c62
aymammeri <ay.mammeri@gmail.com>
2022-02-11 23:21:11 -0500

    Change header color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,7 +24,7 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <Navbar bg='primary' variant='dark' expand='md'>
+  <Navbar bg='dark' variant='dark' expand='md'>
     <Navbar.Brand>
       <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>react-auth-template</Link>
     </Navbar.Brand>
