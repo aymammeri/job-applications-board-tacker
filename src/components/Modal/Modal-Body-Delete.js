f2fda47aa724c98b10f30f55c432c1567cd0d815
aymammeri <ay.mammeri@gmail.com>
2022-02-13 00:21:54 -0500

    Creat Modal Body for coresponding action

--- src/components/Modal/Modal-Body-Delete.js
+++ src/components/Modal/Modal-Body-Delete.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Form } from 'react-bootstrap'
 
-const ModalBody = props => (
-  <Form.Label>You are about to<b>{props.modalTitle}.</b> Are you sure ?</Form.Label>
+const ModalBody = title => (
+  <Form.Label>You are about to<b>{title}.</b> Are you sure ?</Form.Label>
 )
 
 export default ModalBody
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,38 +1,44 @@
 import React from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
 
-// import ModalBodyCreate from './Modal-Body-Create'
-// import ModalBodyEdit from './Modal-Body-Edit'
+import ModalBodyCreateEditColumn from './Modal-Body-Column-EditCreate'
+import ModalBodyCreateEditCell from './Modal-Body-Cell-EditCreate'
 import ModalBodyDelete from './Modal-Body-Delete'
 
 const NewModal = props => {
   const modalType = props.modalType
-  let [modalTitle, buttonText, danger] = []
+  let [modalTitle, modalBody, buttonText, danger] = []
 
   if (modalType === 'create-column') {
     modalTitle = 'New Column'
     buttonText = 'Create Column'
+    modalBody = ModalBodyCreateEditColumn('Enter Name')
   }
   if (modalType === 'create-cell') {
     modalTitle = 'New Cell'
     buttonText = 'Create Cell'
+    modalBody = ModalBodyCreateEditCell('*')
   }
   if (modalType === 'edit-column') {
     modalTitle = 'Edit Column'
     buttonText = 'Submit'
+    modalBody = ModalBodyCreateEditColumn('Enter New Name')
   }
   if (modalType === 'edit-cell') {
     modalTitle = 'Edit Cell'
     buttonText = 'Submit'
+    modalBody = ModalBodyCreateEditCell('')
   }
   if (modalType === 'delete-column') {
     modalTitle = 'Delete The Column'
     buttonText = 'Confirm'
+    modalBody = ModalBodyDelete(modalTitle)
     danger = true
   }
   if (modalType === 'delete-cell') {
     modalTitle = 'Delete The Cell'
     buttonText = 'Confirm'
+    modalBody = ModalBodyDelete(modalTitle)
     danger = true
   }
 
@@ -42,16 +48,16 @@ const NewModal = props => {
         <Modal.Title>{modalTitle}</Modal.Title>
       </Modal.Header>
       <Form>
-        <Modal.Body><ModalBodyDelete modalTitle={modalTitle} /></Modal.Body>
+        <Modal.Body>
+          <Form.Group className='mb-3' controlId='controlInput'>
+            {modalBody}
+          </Form.Group>
+        </Modal.Body>
         <Modal.Footer>
           <Button type='reset' variant='secondary' onClick={props.handleClose}>
             Cancel
           </Button>
-          <Button
-            type='submit'
-            variant={danger && 'danger'}
-            onClick={props.handleClose}
-          >
+          <Button type='submit' variant={danger && 'danger'}>
             {buttonText}
           </Button>
         </Modal.Footer>
