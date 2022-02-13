a2b9546476aa6eff3e55352bbfbe25193281cedd
aymammeri <ay.mammeri@gmail.com>
2022-02-13 17:04:49 -0500

    setApiCall and pass it down

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,46 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Stack, DropdownButton, Dropdown, Card } from 'react-bootstrap'
+import { deleteCell, editCell } from '../../api/crud'
 
 const Cell = props => {
-  const { id, column, setModalType, setElementID, handleShow } = props
-  return (
-    <Card
-      id={id}
-      className='bg-color-primary border border-dark cell'
-      parent={column}
-    >
-      <Stack className='d-flex'>
-        <img src='https://icongr.am/devicon/google-original.svg?size=14&color=currentColor' />
-        <div className='mx-auto text-center pt-2'>Title</div>
-        <DropdownButton
-          variant='link'
-          id='dropdown-basic-button'
-          title={
-            <img src='https://icongr.am/entypo/dots-three-vertical.svg?size=14&color=currentColor' />
-          }
-        >
-          <Dropdown.Item
-            onClick={() => {
-              setModalType('edit-cell')
-              setElementID({ id })
-              handleShow()
-            }}
+  const { id, column, setModalType, setElementID, setApiCall, handleShow } =
+    props
+  const { company } = props
+  const [cellJSX, setCellJSX] = useState(null)
+
+  useEffect(() => {
+    setCellJSX(
+      <Card
+        id={id}
+        className='bg-color-primary border border-dark cell'
+        parent={column}
+      >
+        <Stack className='d-flex'>
+          <img src='https://icongr.am/devicon/google-original.svg?size=14&color=currentColor' />
+          <div className='mx-auto text-center pt-2'>{company}</div>
+          <DropdownButton
+            variant='link'
+            id='dropdown-basic-button'
+            title={
+              <img src='https://icongr.am/entypo/dots-three-vertical.svg?size=14&color=currentColor' />
+            }
           >
-            Edit
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => {
-              setModalType('delete-cell')
-              setElementID({ id })
-              handleShow()
-            }}
-          >
-            Delete
-          </Dropdown.Item>
-        </DropdownButton>
-      </Stack>
-    </Card>
-  )
+            <Dropdown.Item
+              onClick={() => {
+                setModalType('edit-cell')
+                setElementID(...id)
+                setApiCall(editCell)
+                handleShow()
+              }}
+            >
+              Edit Cell
+            </Dropdown.Item>
+            <Dropdown.Item
+              onClick={() => {
+                setModalType('delete-cell')
+                setElementID(...id)
+                setApiCall(deleteCell)
+                handleShow()
+              }}
+            >
+              Delete Cell
+            </Dropdown.Item>
+          </DropdownButton>
+        </Stack>
+      </Card>
+    )
+  }, [Cell])
+  return <>{cellJSX}</>
 }
 
 export default Cell
