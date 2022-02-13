0067fc3932090662f181822b35f55eac3ab09e30
aymammeri <ay.mammeri@gmail.com>
2022-02-12 22:25:24 -0500

    Refactor Modal Completly (DRY)

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,102 +1,63 @@
 import React from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import { Button, Form, Modal } from 'react-bootstrap'
+
+// import ModalBodyCreate from './Modal-Body-Create'
+// import ModalBodyEdit from './Modal-Body-Edit'
+import ModalBodyDelete from './Modal-Body-Delete'
 
 const NewModal = props => {
   const modalType = props.modalType
-  let ModalToRender
+  let [modalTitle, buttonText, danger] = []
 
   if (modalType === 'create-column') {
-    ModalToRender = (
-      <Modal
-        show={props.show}
-        onHide={props.handleClose}
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Create Column Modal</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={props.handleClose}>
-            Close
-          </Button>
-          <Button variant='primary' onClick={props.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    )
+    modalTitle = 'New Column'
+    buttonText = 'Create Column'
   }
   if (modalType === 'create-cell') {
-    ModalToRender = (
-      <Modal
-        show={props.show}
-        onHide={props.handleClose}
-        animation={false}
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Create Cell Modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={props.handleClose}>
-            Close
-          </Button>
-          <Button variant='primary' onClick={props.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    )
+    modalTitle = 'New Cell'
+    buttonText = 'Create Cell'
   }
   if (modalType === 'edit-column') {
-    ModalToRender = (
-      <Modal
-        show={props.show}
-        onHide={props.handleClose}
-        animation={false}
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Edit Column Modal</Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={props.handleClose}>
-            Close
-          </Button>
-          <Button variant='primary' onClick={props.handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    )
+    modalTitle = 'Edit Column'
+    buttonText = 'Submit'
   }
   if (modalType === 'edit-cell') {
-    ModalToRender = (
-      <Modal
-        show={props.show}
-        onHide={props.handleClose}
-        animation={false}
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Edit Cell Modal</Modal.Body>
+    modalTitle = 'Edit Cell'
+    buttonText = 'Submit'
+  }
+  if (modalType === 'delete-column') {
+    modalTitle = 'Delete The Column'
+    buttonText = 'Confirm'
+    danger = true
+  }
+  if (modalType === 'delete-cell') {
+    modalTitle = 'Delete The Cell'
+    buttonText = 'Confirm'
+    danger = true
+  }
+
+  return (
+    <Modal show={props.show} onHide={props.handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>{modalTitle}</Modal.Title>
+      </Modal.Header>
+      <Form>
+        <Modal.Body><ModalBodyDelete modalTitle={modalTitle} /></Modal.Body>
         <Modal.Footer>
-          <Button variant='secondary' onClick={props.handleClose}>
-            Close
+          <Button type='reset' variant='secondary' onClick={props.handleClose}>
+            Cancel
           </Button>
-          <Button variant='primary' onClick={props.handleClose}>
-            Save Changes
+          <Button
+            type='submit'
+            variant={danger && 'danger'}
+            onClick={props.handleClose}
+          >
+            {buttonText}
           </Button>
         </Modal.Footer>
-      </Modal>
-    )
-  }
-  return <>{ModalToRender}</>
+      </Form>
+    </Modal>
+  )
 }
 
 export default NewModal
