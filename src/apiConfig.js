f054b8cfbd6d3abc7ad80d3b8511148731ed677d
aymammeri <ay.mammeri@gmail.com>
2022-02-14 00:54:54 -0500

    Fix Production apiURl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://thawing-anchorage-80200.herokuapp.com/',
+  production: 'https://thawing-anchorage-80200.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
