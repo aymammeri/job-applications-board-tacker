371b93914434ca8b13d444f6ff8bbe5133a53610
aymammeri <ay.mammeri@gmail.com>
2022-02-14 00:49:34 -0500

    Change Production apiURl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://thawing-anchorage-80200.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
