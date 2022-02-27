8c9478392a275ef7b426a0121e776b6b64677aae
aymammeri <ay.mammeri@gmail.com>
2022-02-26 22:29:28 -0500

    remove package-lock

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
   production: 'https://thawing-anchorage-80200.herokuapp.com',
-  development: 'http://localhost:4741'
+  development: 'https://thawing-anchorage-80200.herokuapp.com'
 }
 
 if (window.location.hostname === 'localhost') {
