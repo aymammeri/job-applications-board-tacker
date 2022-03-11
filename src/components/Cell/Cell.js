f10703cd0828fa29429a98b4b1f9d11d84159403
aymammeri <ay.mammeri@gmail.com>
2022-03-11 11:59:00 -0500

    Install react-beautiful-dnd
    Install and implement simple dnd feature without saving to state

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -5,41 +5,57 @@ import { modalActions } from '../../Store/modalSlice/modalReducer'
 
 import { Stack, DropdownButton, Dropdown, Card } from 'react-bootstrap'
 
+import { Draggable } from 'react-beautiful-dnd'
+
 const Cell = props => {
   const dispatch = useDispatch()
   const setupModal = modalActions.setupModal
 
-  const { id, company } = props
+  const { index, id, company } = props
 
   return (
-    <Card id={id} className='bg-color-primary border border-dark cell'>
-      <Stack className='d-flex'>
-        <img src='https://icongr.am/devicon/google-original.svg?size=14&color=currentColor' />
-        <div className='mx-auto text-center pt-2'>{company}</div>
-        <DropdownButton
-          variant='link'
-          id='dropdown-basic-button'
-          title={
-            <img src='https://icongr.am/entypo/dots-three-vertical.svg?size=14&color=currentColor' />
-          }
+    <Draggable draggableId={id} index={index}>
+      {provided => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
         >
-          <Dropdown.Item
-            onClick={() => {
-              dispatch(setupModal({ modalType: 'edit-cell', elementID: id }))
-            }}
-          >
-            Edit Cell
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => {
-              dispatch(setupModal({ modalType: 'delete-cell', elementID: id }))
-            }}
-          >
-            Delete Cell
-          </Dropdown.Item>
-        </DropdownButton>
-      </Stack>
-    </Card>
+          <Card id={id} className='bg-color-primary border border-dark cell'>
+            <Stack className='d-flex'>
+              <img src='https://icongr.am/devicon/google-original.svg?size=14&color=currentColor' />
+              <div className='mx-auto text-center pt-2'>{company}</div>
+              <DropdownButton
+                variant='link'
+                id='dropdown-basic-button'
+                title={
+                  <img src='https://icongr.am/entypo/dots-three-vertical.svg?size=14&color=currentColor' />
+                }
+              >
+                <Dropdown.Item
+                  onClick={() => {
+                    dispatch(
+                      setupModal({ modalType: 'edit-cell', elementID: id })
+                    )
+                  }}
+                >
+                  Edit Cell
+                </Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => {
+                    dispatch(
+                      setupModal({ modalType: 'delete-cell', elementID: id })
+                    )
+                  }}
+                >
+                  Delete Cell
+                </Dropdown.Item>
+              </DropdownButton>
+            </Stack>
+          </Card>
+        </div>
+      )}
+    </Draggable>
   )
 }
 
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -6,6 +6,8 @@ import { modalActions } from '../../Store/modalSlice/modalReducer'
 import { Col, Stack, DropdownButton, Dropdown } from 'react-bootstrap'
 import Cell from '../Cell/Cell.js'
 
+import { Droppable } from 'react-beautiful-dnd'
+
 const Column = props => {
   const dispatch = useDispatch()
   const setupModal = modalActions.setupModal
@@ -13,47 +15,61 @@ const Column = props => {
   const { id, name, cells } = props
 
   return (
-    <Col id={id} className='column border border-dark fluid m-3 pb-3'>
-      <Stack className='d-flex'>
-        <h6 className='text-center pt-2'>{name}</h6>
-        <DropdownButton
-          className='ms-auto'
-          variant='link'
-          id='dropdown-basic-button'
-          title={
-            <img src='https://icongr.am/entypo/dots-three-horizontal.svg?size=14&color=currentColor' />
-          }
+    <Droppable droppableId={id}>
+      {provided => (
+        <Col
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          id={id}
+          className='column border border-dark fluid m-3 pb-3'
         >
-          <Dropdown.Item
-            onClick={() => {
-              dispatch(setupModal({ modalType: 'edit-column', elementID: id }))
-            }}
-          >
-            Rename Column
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => {
-              dispatch(
-                setupModal({ modalType: 'delete-column', elementID: id })
-              )
-            }}
-          >
-            Delete Column
-          </Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item
-            onClick={() => {
-              dispatch(setupModal({ modalType: 'create-cell', elementID: id }))
-            }}
-          >
-            New Cell
-          </Dropdown.Item>
-        </DropdownButton>
-      </Stack>
-      {cells?.map(cell => (
-        <Cell key={cells.indexOf(cell)} id={cell._id} {...cell} />
-      ))}
-    </Col>
+          <Stack className='d-flex'>
+            <h6 className='text-center pt-2'>{name}</h6>
+            <DropdownButton
+              className='ms-auto'
+              variant='link'
+              id='dropdown-basic-button'
+              title={
+                <img src='https://icongr.am/entypo/dots-three-horizontal.svg?size=14&color=currentColor' />
+              }
+            >
+              <Dropdown.Item
+                onClick={() => {
+                  dispatch(
+                    setupModal({ modalType: 'edit-column', elementID: id })
+                  )
+                }}
+              >
+                Rename Column
+              </Dropdown.Item>
+              <Dropdown.Item
+                onClick={() => {
+                  dispatch(
+                    setupModal({ modalType: 'delete-column', elementID: id })
+                  )
+                }}
+              >
+                Delete Column
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item
+                onClick={() => {
+                  dispatch(
+                    setupModal({ modalType: 'create-cell', elementID: id })
+                  )
+                }}
+              >
+                New Cell
+              </Dropdown.Item>
+            </DropdownButton>
+          </Stack>
+          {cells?.map((cell, index) => (
+            <Cell key={index} id={cell._id} {...cell} index={index} />
+          ))}
+          {provided.placeholder}
+        </Col>
+      )}
+    </Droppable>
   )
 }
 
