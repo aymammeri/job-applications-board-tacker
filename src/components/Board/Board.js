aeda476de0b4c4806c9bf79e18b2652fc479ee40
aymammeri <ay.mammeri@gmail.com>
2022-03-11 11:59:00 -0500

    Install react-beautiful-dnd
    Install and implement simple dnd feature without saving to state

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,37 +6,49 @@ import { modalActions } from '../../Store/modalSlice/modalReducer'
 import { Button, Container, Row, Stack } from 'react-bootstrap'
 import Column from '../Column/Column'
 
+import { DragDropContext } from 'react-beautiful-dnd'
+
 const Board = props => {
   const dispatch = useDispatch()
   const setupModal = modalActions.setupModal
 
   const board = useSelector(state => state.board.board)
+  const columns = board.columns
+  console.log(columns)
   const id = board._id
 
+  const onDragEnd = result => {
+
+  }
+
   return (
-    <Container id='board' className='border border-dark'>
-      <Stack className='d-flex'>
-        <div className='text-center pt-2'>Board Job Tracker</div>
-        <Button
-          variant='outline-link'
-          className='ms-auto p-2'
-          onClick={() => {
-            dispatch(setupModal({ modalType: 'create-column', elementID: id }))
-          }}
-        >
-          New Column
-          <img
-            className='ms-2'
-            src='https://icongr.am/clarity/add.svg?size=20&color=currentColor'
-          />
-        </Button>
-      </Stack>
-      <Row>
-        {board.columns.map(col => (
-          <Column key={board.columns.indexOf(col)} id={col._id} {...col} />
-        ))}
-      </Row>
-    </Container>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Container id='board' className='border border-dark'>
+        <Stack className='d-flex'>
+          <div className='text-center pt-2'>Board Job Tracker</div>
+          <Button
+            variant='outline-link'
+            className='ms-auto p-2'
+            onClick={() => {
+              dispatch(
+                setupModal({ modalType: 'create-column', elementID: id })
+              )
+            }}
+          >
+            New Column
+            <img
+              className='ms-2'
+              src='https://icongr.am/clarity/add.svg?size=20&color=currentColor'
+            />
+          </Button>
+        </Stack>
+        <Row>
+          {board.columns.map((col, index) => (
+            <Column key={index} id={col._id} {...col} />
+          ))}
+        </Row>
+      </Container>
+    </DragDropContext>
   )
 }
 
