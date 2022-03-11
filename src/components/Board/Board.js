e01911a87f5ad5234643a102b552ccdb0d56306e
aymammeri <ay.mammeri@gmail.com>
2022-03-10 22:28:22 -0500

    Cleaning code

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { modalActions } from '../../Store/modalSlice/modalReducer'
@@ -13,39 +13,31 @@ const Board = props => {
   const board = useSelector(state => state.board.board)
   const id = board._id
 
-  const [boardJSX, setBoardJSX] = useState(null)
-
-  useEffect(() => {
-    setBoardJSX(
-      <Container id='board' className='border border-dark'>
-        <Stack className='d-flex'>
-          <div className='text-center pt-2'>Board Job Tracker</div>
-          <Button
-            variant='outline-link'
-            className='ms-auto p-2'
-            onClick={() => {
-              dispatch(
-                setupModal({ modalType: 'create-column', elementID: id })
-              )
-            }}
-          >
-            New Column
-            <img
-              className='ms-2'
-              src='https://icongr.am/clarity/add.svg?size=20&color=currentColor'
-            />
-          </Button>
-        </Stack>
-        <Row>
-          {board.columns.map(col => (
-            <Column key={board.columns.indexOf(col)} id={col._id} {...col} />
-          ))}
-        </Row>
-      </Container>
-    )
-  }, [board.columns])
-
-  return <>{boardJSX}</>
+  return (
+    <Container id='board' className='border border-dark'>
+      <Stack className='d-flex'>
+        <div className='text-center pt-2'>Board Job Tracker</div>
+        <Button
+          variant='outline-link'
+          className='ms-auto p-2'
+          onClick={() => {
+            dispatch(setupModal({ modalType: 'create-column', elementID: id }))
+          }}
+        >
+          New Column
+          <img
+            className='ms-2'
+            src='https://icongr.am/clarity/add.svg?size=20&color=currentColor'
+          />
+        </Button>
+      </Stack>
+      <Row>
+        {board.columns.map(col => (
+          <Column key={board.columns.indexOf(col)} id={col._id} {...col} />
+        ))}
+      </Row>
+    </Container>
+  )
 }
 
 export default Board
